a35ccbb02f867123d134a505f7a57e39df3ffcb9
Volik Alexey <alexeiv06@gmail.com>
2025-05-03 14:21:20 +0500

    Выполнение задания module5-task1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { generatePhotoArray } from './data';
+
+const NUM_PHOTOS = 25; // Количество фотографий
+
+// Функция для получения массива сгенерированных объектов
+function getGeneratedPhotos() {
+  return generatePhotoArray(NUM_PHOTOS);
+}
+
+getGeneratedPhotos();
